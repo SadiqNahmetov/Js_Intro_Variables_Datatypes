739f60f021bf755948e80fadd8a65b72d09b2399
SadiqNahmetov <sadigfn@code.edu.az>
2022-08-18 00:04:20 +0400

    Homework Javascript intro

---
+++ asetss/js/script.js
@@ -0,0 +1,156 @@
+// console.log("Salamlar")
+
+
+// var, let, const
+
+//var 
+
+// var name = "Orxan"
+// var name = "Cavid"
+// console.log(name);
+
+// console.log(name + " " + surname);
+// console.log(`${name} ${surname}`);
+
+
+//let
+
+// let name1 = "Orxan";
+// let name2 = "Cavid";
+
+// console.log(name1 + " " + name2);
+
+
+//const
+
+// const age = 50;
+
+// console.log(age);
+
+
+// let age = 55;
+// let surname = "Nuraliyev"
+// let isMaried = true;
+// let address = null;
+// let count;
+
+// console.log(address);
+// console.log(count);
+
+// for (let i = 0; i < students.length; i++) {
+
+//     console.log(students[1])
+
+// }
+//  console.log(person.name + "-" + person.surname);
+
+
+
+// let person1 = {
+//     name: "Orxan",
+//     surname: "Ekberov",
+//     age: 26,
+//     phoneNumbers:[121345465432,564232113]
+// }
+
+// let person2 = {
+//     name: "Orxan",
+//     surname: "Ekberov",
+//     age: 26,
+//     phoneNumbers:[121345465432,564232113]
+
+//  }
+
+// let students = [person1, person2];
+
+
+//   for (let i = 0; i < students.length; i++) {
+        
+//     for (let j = 0; j < students[i].phoneNumbers.length; j++) {
+       
+//         console.log(students[i].phoneNumbers[j]);
+//      }
+        
+// }
+
+
+
+
+// console.log(person.name + " " + person.surname);
+
+// for (let i = 0; i < person1.phoneNumbers.length; i++) {
+//     console.log(person1.phoneNumbers[i]);
+// }
+
+// let numbers = [1,2,3,4,5,6]
+
+// numbers.forEach(item => {
+//     console.log(item);
+// });
+
+// let num1 = 65;
+// let num2 = 33;
+
+// let result = num1 % num2;
+
+// console.log(result);
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i == 0) {
+//     console.log("Ne tek ne de cutdur");
+//   } else {
+//     if (i % 2 == 1) {
+//       console.log("Tek:" + i);
+//     } else {
+//       console.log("Cut:" + i);
+//     }
+//   }
+// }
+
+// let isActive = true;
+
+// if (!isActive) {
+//     console.log("Aktiv deyil");
+// }else{
+//     console.log("Aktivdir");
+// }
+
+// let studentSurname = null;
+
+// let result = studentSurname ?? "Is surname forget";
+
+// let result = isActive ? "Aktivdir" : "Aktiv deyil";
+
+// console.log(result);
+
+// let a = 5;
+// let b = a;
+// b = 8;
+
+// console.log(a);
+// console.log(b);
+
+// let obj1 = {
+//   name: "Orxan",
+//   surname: "Akberov",
+//   age: 26,
+//   phoneNumbers: [123, 321],
+// };
+
+// let obj2 = obj1;
+// obj2.age = 55;
+
+// console.log(obj1);
+
+// let studentName;
+
+// if (studentName == undefined) {
+//     console.log("Teyin olunmayib");
+// }else{
+//     console.log("Teyin olunub");
+// }
+
+// let isDeleted = null;
+
+// console.log(typeof isDeleted);
+    
